144bf0ea78ebd672ff4ea72d1066862bc00775cf
skeuker <skeuker@pnp.co.za>
2018-11-05 19:34:50 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"pnp/sam/model/models"
+], function (UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("pnp.sam.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function () {
+
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// enable routing
+			this.getRouter().initialize();
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+
+		}
+
+	});
+
+});
---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,79 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("pnp.sam.controller.App", {
+
+		//init hook of UI5 controller framework
+		onInit: function () {
+
+		},
+
+		/**
+		 * Event handler for select on navigation item
+		 * @public
+		 * @param {sap.ui.base.Event} oEvent The item select event
+		 */
+		onNavigationItemSelect: function (oEvent) {
+
+			//get selected navigation item 
+			var oNavigationItem = oEvent.getParameter('item');
+
+			//do navigate to seleted item
+			this.doNavigateToSelectedItem(oNavigationItem);
+
+		},
+
+		/**
+		 * Event handler for select on navigation item
+		 * @public
+		 * @param {sap.ui.base.Event} oEvent The item select event
+		 */
+		doNavigateToSelectedItem: function (oNavigationItem) {
+
+			//get selected navigation item and key thereof
+			var sKey = oNavigationItem.getKey();
+
+			//depending on selected navigation item key
+			switch (sKey) {
+
+				//navigate to route of same name
+			case "Workplaces":
+			case "Registers":
+			case "Reports":
+				this.getRouter().navTo(sKey);
+				break;
+
+				//unhandled navigation items
+			default:
+				sap.m.MessageToast.show("Navigation for item key " + sKey + " has not yet been implemented");
+			}
+
+		},
+
+		//toggle side navigation between expanded and condensed
+		onSideNavButtonPress: function () {
+			var oToolPage = this.getView().byId("app");
+			var bSideExpanded = oToolPage.getSideExpanded();
+			this._setToggleButtonTooltip(bSideExpanded);
+			oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+		},
+
+		_setToggleButtonTooltip: function (bSideExpanded) {
+			var oToggleButton = this.getView().byId('sideNavigationToggleButton');
+			if (bSideExpanded) {
+				oToggleButton.setTooltip('Large Size Navigation');
+			} else {
+				oToggleButton.setTooltip('Small Size Navigation');
+			}
+		},
+
+		//convenience method to get Router
+		getRouter: function () {
+			return this.getOwnerComponent().getRouter();
+		},
+
+	});
+
+});
